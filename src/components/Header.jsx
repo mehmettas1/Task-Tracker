a99fd078acf475007f0d912dca429de28d59cb08
mehmet <tas349873@gmail.com>
2022-07-04 22:40:40 +0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,43 @@
-import { useState } from 'react';
-import AddTaskForm from './AddTaskForm';
-
+import AddTaskForm from "./AddTaskForm";
+import { useState } from "react";
 
 const Header = () => {
- const  [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
+  const [btnStyle, setBtnStyle] = useState({
+    name: "SHOW ADD TASK BAR",
+    bgColor: "purple",
+  });
 
- const handleShow = () => {
-  setShow(!show);
- }
+  //? 📢 React doesn't update states instantly, it updates them in bulk. States are updated with async logic to avoid over-rendering
+  const handleShow = () => {
+    if (show) {
+      setBtnStyle({
+        name: "SHOW ADD TASK BAR",
+        bgColor: "purple",
+      });
+    } else {
+      setBtnStyle({
+        name: "CLOSE ADD TASK BAR",
+        bgColor: "red",
+      });
+    }
+    setShow(!show);
+  };
 
-  return <header>
-    <h1>TASK TRACKER</h1>
-    <button onClick={handleShow}>SHOW AND TASK BAR</button>
-   {show && <AddTaskForm/>}
-  </header>;
+  return (
+    <header className="header">
+      <h1>TASK TRACKER</h1>
+      <button
+        onClick={handleShow}
+        className="btn"
+        style={{ backgroundColor: btnStyle.bgColor }}
+      >
+        {btnStyle.name}
+      </button>
+      {show && <AddTaskForm />}
+    </header>
+  );
 };
 
-export default Header;
+export default Header
