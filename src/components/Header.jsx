8e8f29fd638d6254e500e5eb45b202903f2addbd
mehmet <tas349873@gmail.com>
2022-07-04 22:28:36 +0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,20 @@
+import { useState } from 'react';
+import AddTaskForm from './AddTaskForm';
+
+
 const Header = () => {
-  return <header>Header</header>;
+ const  [show, setShow] = useState(false)
+
+
+ const handleShow = () => {
+  setShow(!show);
+ }
+
+  return <header>
+    <h1>TASK TRACKER</h1>
+    <button onClick={handleShow}>SHOW AND TASK BAR</button>
+   {show && <AddTaskForm/>}
+  </header>;
 };
 
 export default Header;
