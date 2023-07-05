d901a80dbb93a5b595cd44856b384c919b4fa852
mehmet <tas349873@gmail.com>
2023-07-05 14:29:34 +0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,12 @@
-import { useState } from "react";
-import AddTaskForm from "./AddTaskForm";
+import AddTaskForm from './AddTaskForm';
+import { useState } from 'react';
+
+const Header = ({ tasks, setTasks }) => {
+  const [show, setShow] = useState(false);
+  const [btnStyle, setBtnStyle] = useState({
+    name: 'SHOW ADD TASK BAR',
+    bgColor: 'purple',
+  });
 
   //! React, default olarak state'leri hemen degistirmeyebilir.
   //! Ekstra render'lari azaltmak icin state'leri toplu (batch) bir sekilde gunceller.
@@ -7,22 +14,38 @@ import AddTaskForm from "./AddTaskForm";
   //! sekilde guncellenmis olur.State'lerin guncelenmesi gelis sirasina gorere yapilir.
   //! Ayni event icerisinde birbirine bagli state'leri kullanirken buna dikkat etmek gerkir.
 
-const Header = ({tasks,setTasks}) => {
-  const [control, setControl] = useState(true)
+  //? https://stackoverflow.com/questions/48563650/does-react-keep-the-order-for-state-updates
 
-  const handleButton =()=>{
-    setControl(!control);
+  const handleShow = () => {
+    if (show) {
+      setBtnStyle({
+        name: 'SHOW ADD TASK BAR',
+        bgColor: 'purple',
+      });
+    } else {
+      setBtnStyle({
+        name: 'CLOSE ADD TASK BAR',
+        bgColor: 'red',
+      });
+    }
+    setShow(!show);
+  };
+  console.log(show);
 
-  }
   return (
-    <header>
-      <h1>Task Tracker</h1>
-      <button onClick={handleButton} type="button" className= {control ? "btn btn-danger" : "btn btn-success"}>
-        {control ?  "Close Add Task Bar" : "Show Add Task Bar" }
+    <header className="header">
+      <h1>TASK TRACKER</h1>
+      <button
+        onClick={handleShow}
+        className="btn"
+        style={{ backgroundColor: btnStyle.bgColor }}
+      >
+        {btnStyle.name}
       </button>
-      {control && <AddTaskForm tasks={tasks} setTasks={setTasks} />}
+      {show && <AddTaskForm tasks={tasks} setTasks={setTasks} />}
     </header>
   );
 };
 
 export default Header;
+
